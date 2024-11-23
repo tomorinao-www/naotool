8f5f4df694a373ee611be98f74fe049b29d144b9
友利奈绪-勿忘我 <1520872279@qq.com>
2024-11-23 15:38:57 +0800

    fix: pageDown

--- naotool/x/xpostx.py
+++ naotool/x/xpostx.py
@@ -198,7 +198,7 @@ async def _get_sub_posts(
             )
         await sub_tab.click()
     # 2.page down
-    for _ in range(limit):
+    for _ in range(2):
         await page.keyboard.press("PageDown")
         await asyncio.sleep(wait_time)  # 给页面加载时间
     # 3.get Locator by xpath
